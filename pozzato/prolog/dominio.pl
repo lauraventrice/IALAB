289821e30c313d92f43caa3770f4c3af7d92d719
Laura <laura.ventrice@edu.unito.it>
2022-03-28 14:41:25 +0200

    chore: commenti e signature dei metodi corretti

--- pozzato/prolog/dominio.pl
+++ pozzato/prolog/dominio.pl
@@ -1,4 +1,5 @@
-/*
+/* Considerazioni iniziali: 
+
 1   2   3
 4   v   5
 6   7   8
@@ -10,8 +11,14 @@ stato iniziale: [1, 2, 3, 4, v, 5, 6, 7, 8]
 
 stato finale: [1, 2, 3, 4, 5, 6, 7, 8, v]
 
+Questa implementazione assume come ipotesi iniziale il fatto che il numero possa comparire una volta sola nella lista!
+nel gioco dell'8 o del 15 ogni numero ovviamente compare una volta sola.
 */
 
+
+
+
+%membership(Elem, List): is true iff Elem is member of the List.  
 membership(_, List) :-
     length(List, 0), fail.
 membership(Elem, [Head|_]) :-
@@ -24,58 +31,61 @@ membership(Elem, [_|Tail]) :-
 iniziale([1, 2, 3, 4, v, 5, 6, 7, 8]).
 finale([1, 2, 3, 4, 5, 6, 7, 8, v]).
 
-% applicabile(indice_posizione_vuota, numero_da_scambiare, StatoAttuale)
-applicabile(0, A, StatoAttuale) :-
-    get_index_wrapper(A, StatoAttuale, IndexOfElement),    % troviamo l'indice dell'elemento da spostare
-    membership(IndexOfElement, [1, 3]).     % se l'indice è uno di quelli scambiabili con la cella vuota 0 allora possiamo spostare.
+/*
+applicabile(IndexEmptyPosition, NumberToMove, CurrentState) :-
+    get_index_wrapper(NumberToMove, CurrentState, IndexOFElement),   --find the index of the element to move
+    membership(IndexOfElement, *list of indexes which can move*).    --if the index is in the list we can move the number
+*/
+applicabile(0, Num, CurrentState) :-
+    get_index_wrapper(Num, CurrentState, IndexOfElement),   
+    membership(IndexOfElement, [1, 3]). 
 
-applicabile(1, A, StatoAttuale) :-
-    get_index_wrapper(A, StatoAttuale, IndexOfElement),    % troviamo l'indice dell'elemento da spostare
-    membership(IndexOfElement, [0, 2, 4]).     % se l'indice è uno di quelli scambiabili con la cella vuota 1 allora possiamo spostare.
+applicabile(1, Num, CurrentState) :-
+    get_index_wrapper(Num, CurrentState, IndexOfElement),    
+    membership(IndexOfElement, [0, 2, 4]).   
 
-applicabile(2, A, StatoAttuale) :-
-    get_index_wrapper(A, StatoAttuale, IndexOfElement),    % troviamo l'indice dell'elemento da spostare
-    membership(IndexOfElement, [1, 5]).     % se l'indice è uno di quelli scambiabili con la cella vuota 2 allora possiamo spostare.
+applicabile(2, Num, CurrentState) :-
+    get_index_wrapper(Num, CurrentState, IndexOfElement),
+    membership(IndexOfElement, [1, 5]).     
 
-applicabile(3, A, StatoAttuale) :-
-    get_index_wrapper(A, StatoAttuale, IndexOfElement),    % troviamo l'indice dell'elemento da spostare
-    membership(IndexOfElement, [0, 4, 6]).     % se l'indice è uno di quelli scambiabili con la cella vuota 3 allora possiamo spostare.
+applicabile(3, Num, CurrentState) :-
+    get_index_wrapper(Num, CurrentState, IndexOfElement),
+    membership(IndexOfElement, [0, 4, 6]).
 
-% applicabile(indice_posizione_vuota, numero_da_scambiare, StatoAttuale)
-applicabile(4, A, StatoAttuale) :-
-    get_index_wrapper(A, StatoAttuale, IndexOfElement),    % troviamo l'indice dell'elemento da spostare
-    membership(IndexOfElement, [1, 3, 4, 6]).     % se l'indice è uno di quelli scambiabili con la cella vuota 4 allora possiamo spostare.
+applicabile(4, Num, CurrentState) :-
+    get_index_wrapper(Num, CurrentState, IndexOfElement),  
+    membership(IndexOfElement, [1, 3, 4, 6]).     
 
-applicabile(5, A, StatoAttuale) :-
-    get_index_wrapper(A, StatoAttuale, IndexOfElement),    % troviamo l'indice dell'elemento da spostare
-    membership(IndexOfElement, [2, 4, 8]).     % se l'indice è uno di quelli scambiabili con la cella vuota 5 allora possiamo spostare.
+applicabile(5, Num, CurrentState) :-
+    get_index_wrapper(Num, CurrentState, IndexOfElement),   
+    membership(IndexOfElement, [2, 4, 8]).    
     
-applicabile(6, A, StatoAttuale) :-
-    get_index_wrapper(A, StatoAttuale, IndexOfElement),    % troviamo l'indice dell'elemento da spostare
-    membership(IndexOfElement, [3, 7]).     % se l'indice è uno di quelli scambiabili con la cella vuota 6 allora possiamo spostare.
+applicabile(6, Num, CurrentState) :-
+    get_index_wrapper(Num, CurrentState, IndexOfElement),   
+    membership(IndexOfElement, [3, 7]). 
         
-applicabile(7, A, StatoAttuale) :-
-    get_index_wrapper(A, StatoAttuale, IndexOfElement),    % troviamo l'indice dell'elemento da spostare
-    membership(IndexOfElement, [4, 6, 8]).     % se l'indice è uno di quelli scambiabili con la cella vuota 7 allora possiamo spostare.
+applicabile(7, Num, CurrentState) :-
+    get_index_wrapper(Num, CurrentState, IndexOfElement),
+    membership(IndexOfElement, [4, 6, 8]).     
         
-applicabile(8, A, StatoAttuale) :-
-    get_index_wrapper(A, StatoAttuale, IndexOfElement),    % troviamo l'indice dell'elemento da spostare
-    membership(IndexOfElement, [5, 7]).     % se l'indice è uno di quelli scambiabili con la cella vuota 8 allora possiamo spostare.
+applicabile(8, Num, CurrentState) :-
+    get_index_wrapper(Num, CurrentState, IndexOfElement), 
+    membership(IndexOfElement, [5, 7]).     
         
+/*
+Return the index of the occurence of the specified element (ElemToFind) in
+ListWhereToFind, or -1if the list does not contain the element. 
+    get_index_wrapper(ElemToFind, ListWhereToFind, ResultIndex)
+*/
 
-% data un elemento di una lista e la lista, restituisce l'indice -> -1 
-% get_index_wrapper(elemento_da_cercare, lista_in_cui_cercare, indice_risultato)
-get_index_wrapper(A, Lista, Index) :- get_index(A, Lista, 0, Index).
+get_index_wrapper(Elem, List, ResultIndex) :- get_index(Elem, List, 0, ResultIndex).
 
-get_index(_, [], _, Res) :-
-    Res is -1, !.
-get_index(A, [Head|_], CurrentIndex, CurrentIndex) :-
-    A == Head, !.
-get_index(A, [_|Tail], CurrentIndex, IndexResult) :-
+get_index(_, [], _, ResultIndex) :-
+    ResultIndex is -1, !.
+get_index(Elem, [Head|_], CurrentIndex, CurrentIndex) :-
+    Elem == Head, !.
+get_index(Elem, [_|Tail], CurrentIndex, ResultIndex) :-
     NewCurrent is CurrentIndex + 1,
-    get_index(A, Tail, NewCurrent, IndexResult).
-
-% questa implementazione assume come ipotesi iniziale il fatto che il numero possa comparire una volta sola nella lista!
-% nel gioco dell'8 o del 15 ogni numero ovviamente compare una volta sola.
+    get_index(Elem, Tail, NewCurrent, ResultIndex).
 
 %swap(elem1, elem2, list, list_result)
